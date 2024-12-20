7167bba339462533cb668a62a8e7b4a3ed8d8235
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2024-12-20 15:32:32 +0530

    feat: for single select radio will be visible instead of checkbox

--- components/SurveyQuestion/index.tsx
+++ components/SurveyQuestion/index.tsx
@@ -10,20 +10,20 @@ export default function SurveyQuestion({ currentQuestion, answers, onChange }: I
             <div className={styles.optionsContainer}>
                 {
                     currentQuestion?.field_type === FIELD_TYPES.MULTIPLE_CHOICE &&
-                    currentQuestion?.options?.map((option: IOption) => (
-                        <label key={option.name} className={styles.optionLabel}>
-                            <input
-                                type="checkbox"
-                                name={option?.name}
-                                title={option?.title}
-                                onChange={(e) => onChange(e, option)}
-                                value={option?.point}
-                                checked={(answers?.[currentQuestion?.name] as MultipleChoiceAnswer)?.[option?.name] !== undefined}
-                                className={styles.optionInput}
-                            />
-                            {option?.title}
-                        </label>
-                    ))
+                        currentQuestion?.options?.map((option: IOption) => (
+                            <label key={option.name} className={styles.optionLabel}>
+                                <input
+                                    type={currentQuestion.multipleSelect ? "checkbox" : "radio"}
+                                    name={option?.name}
+                                    title={option?.title}
+                                    onChange={(e) => onChange(e, option)}
+                                    value={option?.point}
+                                    checked={(answers?.[currentQuestion?.name] as MultipleChoiceAnswer)?.[option?.name] !== undefined}
+                                    className={styles.optionInput}
+                                />
+                                {option?.title}
+                            </label>
+                        ))
                 }
                 {
                     currentQuestion?.field_type === FIELD_TYPES.NUMERICAL &&
